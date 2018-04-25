0f638d1880c2714e900722cc254cfd36a9d7315e
Dominik Manowski <manowski@protomail.com>
2018-04-25 14:22:51 +0200

    Add example - green circle

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#f00";
 ctx.fill();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "#0f0";
+ctx.fill();
+ctx.closePath();
