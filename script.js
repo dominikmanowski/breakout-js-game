caa584fc129ed6570bdc7b5ca1823dd3fa33d98f
Dominik Manowski <manowski@protomail.com>
2018-04-25 16:01:33 +0200

    Add collision detection

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
+
+const ballRadius = 10;
 
 function drawBall() {
 
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -22,6 +24,14 @@ function draw() {
     drawBall();
     x += dx;
     y += dy;
+
+    if(x + dx > canvas.width || x + dx < 0){
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height || y + dy < 0){
+        dy = -dy;
+    }
 
 }
 
