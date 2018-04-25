144ef1b471cbfa21b19918b3d9659b186d4a21ae
Dominik Manowski <manowski@protomail.com>
2018-04-25 14:41:51 +0200

    Change position, size and color of given shapes

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#f00";
+ctx.rect(20, 40, 100, 50);
+ctx.fillStyle = "#459";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
+ctx.arc(240, 160, 50, 0, Math.PI*2, false);
+ctx.fillStyle = "magenta";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0,0 255, .5)";
+ctx.rect(160, 55, 100, 100);
+ctx.strokeStyle = "rgba(0, 66, 255, .5)";
 ctx.stroke();
-ctx.closePath()
+ctx.closePath();
