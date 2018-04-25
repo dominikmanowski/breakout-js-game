a11009583ab22ba583df2543582357193f46e40c
Dominik Manowski <manowski@protomail.com>
2018-04-25 14:05:46 +0200

    Add example - red rectangle

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#f00";
+ctx.fill();
+ctx.closePath();
